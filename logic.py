1f95b90b77e8efb0b752458f605281b8a77a738c
Takafumi Horiuchi <hawkletter080e@icloud.com>
2017-11-08 20:24:30 +0900

    completed making the logic of game

--- logic.py
+++ logic.py
@@ -1,4 +1,5 @@
 #a game of reversi: one-human-player vs ai (on command line)
+import sys
 
 class Board(object):
   board = []
@@ -21,15 +22,106 @@ class Board(object):
     print()
 
   def possibleChoice(self, color):
-    possibility = 0
+    possibility = 0 #number of choices of places where possible to put stone
     for i in range(self.row):
       for j in range(self.col):
-        if self.countUp(self.board[i][j])>0:
-          possibility+=1
+        if self.board[i][j]==self.blank:
+          if self.countUp(i,j,color)>0:
+            possibility+=1
     return possibility
 
-  def gameSet(self):
-    pass
+  def gameSet(self, player_color, ai_color):
+    print("------------------")
+    cnt_player = 0
+    cnt_ai = 0
+    for i in range(self.row):
+      for j in range(self.col):
+        if self.board[i][j]==player_color:
+          cnt_player+=1
+        elif self.board[i][j]==ai_color:
+          cnt_ai+=1
+    print("Player (Black): %d" % cnt_player)
+    print("AI     (White): %d" % cnt_ai)
+    if cnt_player>cnt_ai:
+      print("Winner        : Player")
+    elif cnt_player<cnt_ai:
+      print("Winner        : AI")
+    else:
+      print("Draw")
+    print("------------------")
+    sys.exit()
+
+  def countUp(self, row, col, color):
+    count=0
+    #down
+    for i in range(row+1, self.row):
+      if self.board[i][col]==color:
+        for ii in range(row+1, i):
+          count+=1
+        break
+      elif self.board[i][col]==self.blank:
+        break
+    #right
+    for i in range(col+1, self.col):
+      if self.board[row][i]==color:
+        for ii in range(col+1, i):
+          count+=1
+        break
+      elif self.board[row][i]==self.blank:
+        break
+    #up
+    for i in range(row-1,-1,-1):
+      if self.board[i][col]==color:
+        for ii in range(row-1,i,-1):
+          count+=1
+        break
+      elif self.board[i][col]==self.blank:
+        break
+    #left
+    for i in range(col-1,-1,-1):
+      if self.board[row][i]==color:
+        for ii in range(col-1,i,-1):
+          count+=1
+        break
+      elif self.board[row][i]==self.blank:
+        break
+    #left-upper
+    limit = min(row, col)
+    for i in range(1,limit+1):
+      if self.board[row-i][col-i]==color:
+        for ii in range(1,i):
+          count+=1
+        break
+      elif self.board[row-1][col-1]==self.blank:
+        break
+    #right-lower
+    limit = min(self.row-row, self.col-col)
+    for i in range(1,limit):
+      if self.board[row+i][col+i]==color:
+        for ii in range(1,i):
+          count+=1
+        break
+      elif self.board[row+i][col+i]==self.blank:
+        break
+    #left-lower
+    limit = min(self.row-row, col+1)
+    for i in range(1,limit):
+      if self.board[row+i][col-i]==color:
+        for ii in range(1,i):
+          count+=1
+        break
+      elif self.board[row+i][col-i]==self.blank:
+        break
+    #right-upper
+    limit = min(row+1, self.col-col)
+    for i in range(1,limit):
+      if self.board[row-i][col+i]==color:
+        for ii in range(1,i):
+          count+=1
+        break
+      elif self.board[row-i][col+i]==self.blank:
+        break
+    return count
 
 
 class Player(object):
@@ -47,7 +139,7 @@ class Player(object):
       print("invalid input (out of range)")
       self.selectPoint()
     elif self.my_board.board[self.row][self.col]==my_board.blank:
-      if self.countUp(self.row, self.col)>0:
+      if my_board.countUp(self.row, self.col, self.color)>0:
         self.my_board.board[self.row][self.col] = self.color
       else:
         print("invalid input (unflippable)")
@@ -127,78 +219,6 @@ class Player(object):
       elif my_board.board[self.row-i][self.col+i]==my_board.blank:
         break
 
-  def countUp(self, row, col):
-    count=0
-    #down
-    for i in range(row+1, my_board.row):
-      if my_board.board[i][col]==self.color:
-        for ii in range(row+1, i):
-          count+=1
-        break
-      elif my_board.board[i][col]==my_board.blank:
-        break
-    #right
-    for i in range(col+1, my_board.col):
-      if my_board.board[row][i]==self.color:
-        for ii in range(col+1, i):
-          count+=1
-        break
-      elif my_board.board[row][i]==my_board.blank:
-        break
-    #up
-    for i in range(row-1,-1,-1):
-      if my_board.board[i][col]==self.color:
-        for ii in range(row-1,i,-1):
-          count+=1
-        break
-      elif my_board.board[i][col]==my_board.blank:
-        break
-    #left
-    for i in range(col-1,-1,-1):
-      if my_board.board[row][i]==self.color:
-        for ii in range(col-1,i,-1):
-          count+=1
-        break
-      elif my_board.board[row][i]==my_board.blank:
-        break
-    #left-upper
-    limit = min(row, col)
-    for i in range(1,limit+1):
-      if my_board.board[row-i][col-i]==self.color:
-        for ii in range(1,i):
-          count+=1
-        break
-      elif my_board.board[row-1][col-1]==my_board.blank:
-        break
-    #right-lower
-    limit = min(my_board.row-row, my_board.col-col)
-    for i in range(1,limit):
-      if my_board.board[row+i][col+i]==self.color:
-        for ii in range(1,i):
-          count+=1
-        break
-      elif my_board.board[row+i][col+i]==my_board.blank:
-        break
-    #left-lower
-    limit = min(my_board.row-row, col+1)
-    for i in range(1,limit):
-      if my_board.board[row+i][col-i]==self.color:
-        for ii in range(1,i):
-          count+=1
-        break
-      elif my_board.board[row+i][col-i]==my_board.blank:
-        break
-    #right-upper
-    limit = min(row+1, my_board.col-col)
-    for i in range(1,limit):
-      if my_board.board[row-i][col+i]==self.color:
-        for ii in range(1,i):
-          count+=1
-        break
-      elif my_board.board[row-i][col+i]==my_board.blank:
-        break
-    return count
-
 
 class ArtificialIntelligence(Player):
   def aiCalculate(self):
@@ -207,7 +227,7 @@ class ArtificialIntelligence(Player):
     for i in range(2):
       for j in range(2):
         if my_board.board[i*(my_board.row-1)][j*(my_board.col-1)]==my_board.blank:
-          count = self.countUp(i*(my_board.row-1),j*(my_board.col-1))
+          count = my_board.countUp(i*(my_board.row-1),j*(my_board.col-1),self.color)
           if count>max_count:
             max_count = count
             self.row = i*(my_board.row-1)
@@ -220,7 +240,7 @@ class ArtificialIntelligence(Player):
       for i in range(my_board.row):
         for j in range(my_board.col):
           if my_board.board[i][j]==my_board.blank:
-            count = self.countUp(i,j)
+            count = my_board.countUp(i,j,self.color)
             if count>max_count:
               max_count = count
               self.row = i
@@ -246,15 +266,14 @@ while True:
     my_player.selectPoint()
   else:
     if not my_board.possibleChoice(my_ai.color)>0:
-      my_board.gameSet()
+      my_board.gameSet(my_player.color, my_ai.color)
   if my_board.possibleChoice(my_ai.color)>0:
     my_board.printBoard()
     my_ai.aiCalculate()
   else:
     if not my_board.possibleChoice(my_player.color)>0:
-      my_board.gameSet()
+      my_board.gameSet(my_player.color, my_ai.color)
 
 """
 - end process
-- function countUp() should have been defined in class Board
 """
