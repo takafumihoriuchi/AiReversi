806a52921ce966fc05a79f01176519a72bd17b14
Takafumi Horiuchi <hawkletter080e@icloud.com>
2017-11-08 10:48:57 +0900

    created the moves of ai, choosing the place with the highest return in that round only, with corners with the highest priority

--- logic.py
+++ logic.py
@@ -20,6 +20,9 @@ class Board(object):
       print("  ".join(i))
     print()
 
+  def endCheck(self, color):
+    pass
+
 
 class Player(object):
   def __init__(self, color, my_board):
@@ -35,7 +38,7 @@ class Player(object):
     if self.row not in range(self.my_board.row) or self.col not in range(self.my_board.col):
       print("invalid input (out of range)")
       self.selectPoint()
-    elif self.my_board.board[self.row][self.col]=="O":
+    elif self.my_board.board[self.row][self.col]==my_board.blank:
       self.my_board.board[self.row][self.col] = self.color
     else:
       print("invalid input (already taken)")
@@ -114,8 +117,106 @@ class Player(object):
 
 
 class ArtificialIntelligence(Player):
-  pass
+  def aiCalculate(self):
+    #check the four-corners first
+    max_count = 0
+    for i in range(2):
+      for j in range(2):
+        if my_board.board[i*(my_board.row-1)][j*(my_board.col-1)]==my_board.blank:
+          count = self.countUp(i,j)
+          if count>max_count:
+            max_count = count
+            self.row = i
+            self.col = j
+    if max_count>0:
+      self.putStone()
+    else:
+      #select point to get highest return in that round (if multiple choice, select point that appears first)
+      max_count = 0
+      for i in range(my_board.row):
+        for j in range(my_board.col):
+          if my_board.board[i][j]==my_board.blank:
+            count = self.countUp(i,j)
+            if count>max_count:
+              max_count = count
+              self.row = i
+              self.col = j
+      if max_count>0: #(could also be written " if max_count: " )
+        self.putStone()
+      else: #if there are no possible choices
+        my_board.endCheck(self.color)
 
+  def countUp(self, row, col):
+    count=0
+    #down
+    for i in range(row+1, my_board.row):
+      if my_board.board[i][col]==self.color:
+        for ii in range(row+1, i):
+          count+=1
+        break
+      elif my_board.board[i][col]==my_board.blank:
+        break
+    #right
+    for i in range(col+1, my_board.col):
+      if my_board.board[row][i]==self.color:
+        for ii in range(col+1, i):
+          count+=1
+        break
+      elif my_board.board[row][i]==my_board.blank:
+        break
+    #up
+    for i in range(row-1,-1,-1):
+      if my_board.board[i][col]==self.color:
+        for ii in range(row-1,i,-1):
+          count+=1
+          break
+      elif my_board.board[i][col]==my_board.blank:
+        break
+    #left
+    for i in range(col-1,-1,-1):
+      if my_board.board[row][i]==self.color:
+        for ii in range(col-1,i,-1):
+          count+=1
+        break
+      elif my_board.board[row][i]==my_board.blank:
+        break
+    #left-upper
+    limit = min(row, col)
+    for i in range(1,limit+1):
+      if my_board.board[row-i][col-i]==self.color:
+        for ii in range(1,i):
+          count+=1
+        break
+      elif my_board.board[row-1][col-1]==my_board.blank:
+        break
+    #right-lower
+    limit = min(my_board.row-row, my_board.col-col)
+    for i in range(1,limit):
+      if my_board.board[row+i][col+i]==self.color:
+        for ii in range(1,i):
+          count+=1
+        break
+      elif my_board.board[row+i][col+i]==my_board.blank:
+        break
+    #left-lower
+    limit = min(my_board.row-row, col+1)
+    for i in range(1,limit):
+      if my_board.board[row+i][col-i]==self.color:
+        for ii in range(1,i):
+          count+=1
+        break
+      elif my_board.board[row+i][col-i]==my_board.blank:
+        break
+    #right-upper
+    limit = min(row+1, my_board.col-col)
+    for i in range(1,limit):
+      if my_board.board[row-i][col+i]==self.color:
+        for ii in range(1,i):
+          count+=1
+        break
+      elif my_board.board[row-i][col+i]==my_board.blank:
+        break
+    return count
 
 my_board = Board(8,8,"O")
 my_board.createBoard()
@@ -129,5 +230,13 @@ my_player = Player("B", my_board) #set my_player.color to "B" to represent black
 my_ai = ArtificialIntelligence("W", my_board) #set my_ai.color to "W" to represent white
 
 while True:
+  #endCheck("B") for player comes here; how to branch
   my_player.selectPoint()
   my_board.printBoard()
+  my_ai.aiCalculate()
+  my_board.printBoard()
+
+"""
+- only allow player to put in places where they can flip stones
+- end process
+"""
